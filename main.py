35e92e259fe87535e557057a21ac01facbf9f86e
Pamado-23 <pamado1@my.vcccd.edu>
2024-06-26 16:00:22 -0700

    Lab 2-3

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def main():
     total 
     average
     """
+    # Input three values
+    val1 = int(input())
+    val2 = int(input())
+    val3 = int(input())
+
+    # Calculate the total
+    total = val1 + val2 + val3
+
+    #Calulate the average
+    average = total / 3
+
+    # Print the Values, Total, and Average
+    print (f'Values: {val1} {val2} {val3}')
+    print (f'Total: {total}')
+    print (f'Average: {average:.2f}')
 
     ########################################
     # Do not delete the return statement
